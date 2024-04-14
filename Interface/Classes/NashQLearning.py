f14412db88965a080f7c3c06dca8757b84c3782b
andrea-809 <128220234+andrea-809@users.noreply.github.com>
2024-04-14 17:24:40 +0200

    NashQLearning class

---
+++ Interface/Classes/NashQLearning.py
@@ -0,0 +1,35 @@
+import numpy as np
+import random as rand
+import nashpy as nash
+import ipywidgets as widgets
+import IPython.display as display
+
+class NahQLearning:
+    def __init__(self, n_players, n_games, action_per_player, transition_matrix, reward_matrix) -> None:
+        self.n_players = n_players
+        self.n_games = n_games
+        self.action_per_player = action_per_player
+        self.transition_matrix = transition_matrix
+        self.reward_matrix = reward_matrix 
+        
+        self.gamesLoadingBarNashQ = widgets.IntProgress(
+        value=0,
+        min=0,
+        max=n_games-1,
+        step=1,
+        description='Games:',
+        bar_style='info',
+        ) 
+        display(self.gamesLoadingBarNashQ)
+
+    def computeNashEq(self, state, payoff_matrix):
+        game = nash.Game(payoff_matrix[state, :, :, 0], self.reward_matrix[state, :, :, 1])
+        eqs = game.vertex_enumeration()
+        try:
+            eq = next(eqs)
+            return np.abs(eq)
+        except Exception:
+            return [[1, 0], [0, 1]]
+
+    def reward(state, player1_action, player2_action, payoffMatrix):
+        return payoffMatrix[state, player1_action, player2_action]
