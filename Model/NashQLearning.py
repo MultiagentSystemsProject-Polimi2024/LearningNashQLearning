1da1851e1d15eb7a712b0ddaf5127832b26e8fee
andrea-809 <128220234+andrea-809@users.noreply.github.com>
2024-04-20 19:32:26 +0200

    Added Reset possibility

--- Model/NashQLearning.py
+++ Model/NashQLearning.py
@@ -5,12 +5,13 @@ import ipywidgets as widgets
 import IPython.display as display
 
 class NahQLearning:
-    def __init__(self, n_players, n_games, action_per_player, transition_matrix, reward_matrix) -> None:
+    def __init__(self, n_players, n_games, action_per_player, transition_matrix, reward_matrix, goal_state = none) -> None:
         self.n_players = n_players
         self.n_games = n_games
         self.action_per_player = action_per_player
         self.transition_matrix = transition_matrix
         self.reward_matrix = reward_matrix 
+        self.goal_state = goal_state 
         
         #widget
         self.gamesLoadingBarNashQ = widgets.IntProgress(
@@ -86,7 +87,7 @@ class NahQLearning:
 
 
     #NashQ learning algorithm
-    def nashQlearning(self, episodes, alfa, gamma, epsilon, pure_training_ep, decaying_epsilon):
+    def nashQlearning(self, episodes, alfa, gamma, epsilon, pure_training_ep, decaying_epsilon, reset = False):
         
         #declare qTables
         qTables = [np.zeros((self.n_games, self.action_per_player, self.action_per_player, self.n_players)) for _ in range(self.n_players)]
@@ -106,34 +107,52 @@ class NahQLearning:
             alfa = alfa / (t + 1 - pure_training_ep) if t >= pure_training_ep else alfa
             epsilon = epsilon / (t + 1 - decaying_epsilon) if t >= decaying_epsilon else epsilon
 
+            #choose action
             player1_action = np.random.choice(self.action_per_player, p=nashEq[0]) if np.random.rand() > epsilon else np.random.choice(self.action_per_player)
             player2_action = np.random.choice(self.action_per_player, p=nashEq[1]) if np.random.rand() > epsilon else np.random.choice(self.action_per_player)
+            
+            #calculating next state
+            if(reset and self.goal_state != None and state == self.goal_state):
+                next_state = 0
+                """be cautious to the qTable updates... in case u wanna delete this just delete also reset par"""
+            else:
+                next_state = np.random.choice(range(self.n_games), p=self.transition_matrix[player1_action, player2_action, state])
 
-            next_state = np.random.choice(range(self.n_games), p=self.transition_matrix[player1_action, player2_action, state])
-
+            #getting reward for the current move
             r = self.reward(state, player1_action, player2_action, self.reward_matrix)
             
             for i in range(self.n_players):
+                #get qTable for the player i
                 qTable = qTables[i]
 
+                """is this necessary???"""
+                #compute Nash Equilibrium for the current state and memorize it
                 nashEq = np.abs(self.computeNashEq(state, qTable, qTable))
-
                 NashEquilibria[i].append(nashEq)
 
+                #compute the expected payoff for the next state
                 next_NashEq = self.computeNashEq(next_state, qTable, qTable)
                 next_qVal_0 = self.expectedPayoff(qTable[next_state, :, :, 0], next_NashEq[0], next_NashEq[1])
                 next_qVal_1 = self.expectedPayoff(qTable[next_state, :, :, 1], next_NashEq[0], next_NashEq[1])
                 
+                #copy qTable
                 oldQ = qTable[state, player1_action, player2_action].copy()
                 
+                #update qTable
                 qTable[state, player1_action, player2_action, 0] = (1 - alfa) * qTable[state, player1_action, player2_action, 0] + alfa * (r[0] + gamma * next_qVal_0)
                 qTable[state, player1_action, player2_action, 1] = (1 - alfa) * qTable[state, player1_action, player2_action, 1] + alfa * (r[1] + gamma * next_qVal_1)
 
+                #memorize the difference between the old and the new value in the qTable
                 diffs[i].append(qTable[state, player1_action, player2_action] - oldQ)
                 
+                #update the total reward of the player i
                 totalReward[i] += r
+                #memorize the reward of the player i
                 NashQRewards[i].append(r)
-                print(qTables[i])
+            
+            #update the state
             state = next_state
+
+            #update the loading bar
             self.gamesLoadingBarNashQ.value += 1
         return totalReward, diffs, NashQRewards, NashEquilibria
