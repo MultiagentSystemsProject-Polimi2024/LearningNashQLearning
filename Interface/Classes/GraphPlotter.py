7d30158059f22dac6c51b17cf8f7b0d7db8b5885
Federico Rocca <fede.rock.2002@gmail.com>
2024-04-30 10:38:22 +0200

    fixed attribute

--- Interface/Classes/GraphPlotter.py
+++ Interface/Classes/GraphPlotter.py
@@ -14,4 +14,4 @@ class PlotGraph:
             self.node_colors = node_colors
 
     def plot(self):
-        Graph(G, node_labels=True, node_layout='circular', edge_labels=self.edge_labels, edge_label_fontdict=dict(size=5, fontweight='bold'), edge_layout='arc', node_size=6, edge_width=0.5, arrows=True, ax=self.ax, node_edge_color=self.node_colors, node_label_fontdict=dict(size=10), edge_label_position=0.2, edge_labels_rotate=False)
+        Graph(self.graph, node_labels=True, node_layout='circular', edge_labels=self.edge_labels, edge_label_fontdict=dict(size=5, fontweight='bold'), edge_layout='arc', node_size=6, edge_width=0.5, arrows=True, ax=self.ax, node_edge_color=self.node_colors, node_label_fontdict=dict(size=10), edge_label_position=0.2, edge_labels_rotate=False)
