c8d0e832b66e5915be152090269329525587eb99
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-28 11:29:40 +0200

    chore: pubblication setup

--- LearningNashQLearning/setup.py
+++ LearningNashQLearning/setup.py
@@ -5,5 +5,11 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'networkx',
+        'pygambit',
+        'netgraph'
     ]
 )
