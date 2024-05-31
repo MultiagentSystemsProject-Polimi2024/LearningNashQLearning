bd434173bc5fe5c8a649818124fcde797e0944bb
Paolo Ginefra <ginefra.paolo@gmail.com>
2024-05-31 13:57:36 +0200

    chore: uopdate package

--- LearningNashQLearning/setup.py
+++ LearningNashQLearning/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='LearningNashQLearning',
-    version='0.2',
+    version='0.4',
     packages=find_packages(),
     description='A library for learning NashQ-learning',
     install_requires=[
